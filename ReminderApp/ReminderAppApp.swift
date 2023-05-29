fd564d9d4a421c19fcb0bfcd74d570e2ff7b5a3d
canknbr <cankanbur1@gmail.com>
2023-05-29 20:09:26 +0300

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Can Kanbur on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
